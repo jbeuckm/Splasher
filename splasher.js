4502f45c829c2662eb1f5f7eed7d0f543373c830
joe beuckman <joebeuckman@gmail.com>
2013-05-09 11:52:24 -0500

    initial commit

---
+++ splasher.js
@@ -0,0 +1,86 @@
+#!/usr/bin/env phantomjs
+
+var htmlFile = '';
+
+/**
+ * Identify the HTML file that will be used to render the splash images.
+ */
+var args = require('system').args;
+if (args.length === 1) {
+  console.log('Pass the path of the HTML file as the first argument.');
+  phantom.exit();
+}
+else {
+  htmlFile = args[1];
+}
+
+/**
+ * Load the splash pages size specs.
+ */
+var defaults = require('./defaults.json');
+
+var page = require('webpage').create();
+
+page.onConsoleMessage = function(msg) {
+  console.log(msg);
+};
+
+
+/**
+ * When the frame wrapper html loads, set the iframe src to the splash page html and stop responding to LoadFinished.
+ * @param status
+ */
+page.onLoadFinished = function(status) {
+
+  if ( status === "success" ) {
+
+    page.evaluate(function(htmlFile) {
+      $('#splash-frame').prop('src', htmlFile);
+    }, htmlFile);
+
+    processNextDefaultSize();
+  }
+  else {
+    console.log(status);
+    phantom.exit();
+  }
+
+  page.onLoadFinished = null;
+};
+
+page.open('./frame.html');
+
+
+/**
+ * Resize the iframe to the next specification, wait for phantom to redraw, and render the named image.
+ */
+function processNextDefaultSize() {
+
+  if (defaults.length == 0) {
+    phantom.exit();
+    return;
+  }
+
+  var size = defaults.shift();
+
+
+  page.evaluate(function(size) {
+
+    $('#splash-frame').prop('width', size.width).prop('height', size.height);
+
+    console.log('size = '+JSON.stringify(size));
+
+  }, size);
+
+
+  page.clipRect = { left: 0, top: 0, width: size.width, height: size.height };
+
+  setTimeout(function(){
+
+    page.render(size.filename);
+
+    processNextDefaultSize();
+
+  }, 500);
+
+}
