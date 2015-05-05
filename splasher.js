1169ce30ee02ae8beb820ca9fa7fae3a52224f92
joe <joseph.beuckman@usbank.com>
2015-05-05 13:25:05 -0500

    added apple watch sizes

--- splasher.js
+++ splasher.js
@@ -8,12 +8,11 @@ var configFile;
  */
 var args = require('system').args;
 if (args.length === 1) {
-  console.log('Pass the path of the HTML file as the first argument.');
-  phantom.exit();
-}
-else {
-  htmlFile = args[1];
-  configFile = args[2] || './defaults.json';
+    console.log('Pass the path of the HTML file as the first argument.');
+    phantom.exit();
+} else {
+    htmlFile = args[1];
+    configFile = args[2] || './defaults.json';
 }
 
 /**
@@ -23,8 +22,8 @@ var config = require(configFile);
 
 var page = require('webpage').create();
 
-page.onConsoleMessage = function(msg) {
-  console.log(msg);
+page.onConsoleMessage = function (msg) {
+    console.log(msg);
 };
 
 
@@ -32,22 +31,21 @@ page.onConsoleMessage = function(msg) {
  * When the frame wrapper html loads, set the iframe src to the splash page html and stop responding to LoadFinished.
  * @param status
  */
-page.onLoadFinished = function(status) {
+page.onLoadFinished = function (status) {
 
-  if ( status === "success" ) {
+    if (status === "success") {
 
-    page.evaluate(function(htmlFile) {
-      $('#splash-frame').prop('src', htmlFile);
-    }, htmlFile);
+        page.evaluate(function (htmlFile) {
+            $('#splash-frame').prop('src', htmlFile);
+        }, htmlFile);
 
-    processNextDefaultSize();
-  }
-  else {
-    console.log(status);
-    phantom.exit();
-  }
+        processNextDefaultSize();
+    } else {
+        console.log(status);
+        phantom.exit();
+    }
 
-  page.onLoadFinished = null;
+    page.onLoadFinished = null;
 };
 
 page.open('./frame.html');
@@ -58,31 +56,36 @@ page.open('./frame.html');
  */
 function processNextDefaultSize() {
 
-  if (config.length == 0) {
-    phantom.exit();
-    return;
-  }
+    if (config.length == 0) {
+        phantom.exit();
+        return;
+    }
 
-  var size = config.shift();
-
-
-  page.evaluate(function(size) {
-
-    $('#splash-frame').prop('width', size.width).prop('height', size.height);
-
-    console.log('size = '+JSON.stringify(size));
-
-  }, size);
+    var size = config.shift();
 
 
-  page.clipRect = { left: 0, top: 0, width: size.width, height: size.height };
+    page.evaluate(function (size) {
 
-  setTimeout(function(){
+        $('#splash-frame').prop('width', size.width).prop('height', size.height);
 
-    page.render('out/'+size.filename);
+        console.log('size = ' + JSON.stringify(size));
 
-    processNextDefaultSize();
+    }, size);
 
-  }, 1000);
+
+    page.clipRect = {
+        left: 0,
+        top: 0,
+        width: size.width,
+        height: size.height
+    };
+
+    setTimeout(function () {
+
+        page.render('out/' + size.filename);
+
+        processNextDefaultSize();
+
+    }, 1);
 
 }
